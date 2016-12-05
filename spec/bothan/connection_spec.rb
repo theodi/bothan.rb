eb12a2255dab6ef28b29096966de2f28dd1339c4
pezholio <pezholio@gmail.com>
2016-12-05 11:41:26 +0000

    Add tests for metrics

--- spec/bothan/connection_spec.rb
+++ spec/bothan/connection_spec.rb
@@ -3,12 +3,23 @@ require 'spec_helper'
 module Bothan
   describe Connection do
 
-    it 'sets username, password and endpoint' do
-      bothan = Bothan::Connection.new('username', 'password', 'http://bothan.example.org')
+    before(:each) do
+      @bothan = Bothan::Connection.new('username', 'password', 'http://bothan.example.org')
+    end
 
-      expect(bothan.instance_variable_get('@username')).to eq('username')
-      expect(bothan.instance_variable_get('@password')).to eq('password')
-      expect(bothan.instance_variable_get('@endpoint')).to eq('http://bothan.example.org')
+    it 'sets username, password and endpoint' do
+      expect(@bothan.instance_variable_get('@username')).to eq('username')
+      expect(@bothan.instance_variable_get('@password')).to eq('password')
+      expect(@bothan.instance_variable_get('@endpoint')).to eq('http://bothan.example.org')
+    end
+
+    it 'returns a metrics instance' do
+      expect(@bothan.metrics).to be_an_instance_of(Bothan::Metrics)
+    end
+
+    it 'sets the right credentials' do
+      expect(Bothan::Metrics).to receive(:new).with('username', 'password', 'http://bothan.example.org')
+      @bothan.metrics
     end
 
   end
